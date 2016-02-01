6e733d4219f1bc5971615e5f6046625ee688c384
ashuforweb <ashuforweb@gmail.com>
2016-01-31 20:33:17 -0800

    added clock

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 var runs = 0;
 var wickets = 0;
-window.onload = function(){
-  document.getElementById("scr").innerHTML = runs+"/"+wickets;
+// var intervalID;
+window.onload = function() {
+  document.getElementById("scr").innerHTML = runs + "/" + wickets;
+  setInterval(function() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes();
+    var seconds = date.getSeconds() < 10 ? '0'+date.getSeconds() : date.getSeconds();
+    document.getElementById("tme").innerHTML = hours + ":" + minutes + ":" + seconds;
+  }, 1000);
 };
